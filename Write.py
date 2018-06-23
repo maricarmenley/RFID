47684477a3fe3546bbae83dd5a6f23ee6e7ee9b0
Maricarmen <maricarmenleyf@gmail.com>
2018-06-23 15:33:01 -0700

    Update Write.py

--- Write.py
+++ Write.py
@@ -73,7 +73,7 @@ while continue_reading:
         if status == MIFAREReader.MI_OK:
 
             # Variable for the data to write
-            data = [010 020 030 040 050 060 070 080 090 100 110 120 130 140 150 160]
+            data = [111 121 131 141 151 161 171 181 191 100 110 120 130 140 150 160]
 
             # Fill the data with 0xFF
             for x in range(0,16):
